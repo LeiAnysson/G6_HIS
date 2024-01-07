28d1f919c0e3282ba31a6e717148c46c4e83482d
patotiee <155729371+aliesterkyutie@users.noreply.github.com>
2024-01-07 12:34:07 +0800

    Add files via upload

--- ListOfStaff.java
+++ ListOfStaff.java
@@ -1,115 +1,162 @@
+
+package listofstaff;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ListOfStaff {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    List <Staffs> name = new ArrayList<>();
+  
+    //Doctors
+    name.add(new Doctor(" Dr. John Smith,"));
+    name.add(new Doctor(" Dr. Mikko Speed "));
+    name.add(new Doctor1(" Dr. James West"));
+    name.add(new Doctor2(" Dr. Ray Mundo"));
+    name.add(new Doctor1(" Dr. Shawn Monroe"));
+    name.add(new Doctor2(" Dr. John Ellis"));  
+    name.add(new Doctor(" Dr. Kenzo Robert"));  
+    name.add(new Doctor1(" Dr. Yohan Perez"));
+    name.add(new Doctor2(" Dr. Kael Velasquez"));
+    name.add(new Doctor1(" Dr. Lash Rodriguez"));
+    
+    //Nurse
+    name.add(new Nurse("Nurse. Janne Barloe"));
+    name.add(new Nurse1("Nurse. Smith Caddle"));
+    name.add(new Nurse2("Nurse. Amy Hart"));
+    name.add(new Nurse("Nurse. Stella Katz"));
+    name.add(new Nurse1("Nurse. Cassy laurier"));
+    name.add(new Nurse2("Nurse. Kath Santiago"));
+    name.add(new Nurse("Nurse. Cath Madden"));
+    name.add(new Nurse1("Nurse. Mavis Elrod"));
+    name.add(new Nurse2("Nurse. Fiora Hansley"));
+    name.add(new Nurse("Nurse. May Monroe, "));
+    
+    //Maintenance
+    name.add(new Maintenance("Maintenace, Simon Ajello "));
+    name.add(new Maintenance1("Maintenace, Benjamin Barone "));
+    name.add(new Maintenance2("Maintenace, Lucas Bernard"));
+    name.add(new Maintenance1("Maintenace, Noah Santos"));
+    name.add(new Maintenance2("Maintenace, Liam De Leon "));
+    name.add(new Maintenance1("Maintenace, Oliver Cruz ")); 
+    name.add(new Maintenance("Maintenace, William Aquino"));
+    name.add(new Maintenance("Maintenace, Henry Chavez"));
+    name.add(new Maintenance2("Maintenace, Mateo Abalos"));  
+    name.add(new Maintenance("Maintenace, Ethan Gonzales"));
+    
+      System.out.println("---------------Staffs And Maintinance---------------");
+    
+      
+      System.out.println("Please Choose a Category: ");
+      System.out.println("[1] Doctor");
+      System.out.println("[2] Nurse");
+      System.out.println("[3] Maintenanace");
+      
+      Set<Integer> chosenCategories = new HashSet<>();
 
-	public void ListOfStaff() {
-		Scanner input = new Scanner(System.in);
-
-		List<Staff> name = new ArrayList<>();
-
-		// Doctors
-		name.add(new Doctor(" Dr. John Smith", "Orthopedic"));
-		name.add(new Doctor(" Dr. Mikko Speed ", "General Surgery"));
-		name.add(new Doctor(" Dr. James West", "Pediatrics"));
-		name.add(new Doctor(" Dr. Ray Mundo", "Cardiology"));
-		name.add(new Doctor(" Dr. Shawn Monroe", ""));
-		name.add(new Doctor(" Dr. John Ellis", "Pulmonologist"));
-		name.add(new Doctor(" Dr. Kenzo Robert", "Gastroenterologists"));
-		name.add(new Doctor(" Dr. Yohan Perez", "Neurologist"));
-		name.add(new Doctor(" Dr. Kael Velasquez", "Psychiatrist"));
-		name.add(new Doctor(" Dr. Lash Rodriguez", "Rheumatologist"));
-
-		// Nurse
-		name.add(new Nurse("Nurse. Janne Barloe", "Registered Nurse"));
-		name.add(new Nurse("Nurse. Smith Caddle", "Registered Nurse"));
-		name.add(new Nurse("Nurse. Amy Hart", "Registered Nurse"));
-		name.add(new Nurse("Nurse. Stella Katz", "Critical Care Nurse"));
-		name.add(new Nurse("Nurse. Cassy laurier", "Critical Care Nurse"));
-		name.add(new Nurse("Nurse. Kath Santiago", "Critical Care Nurse"));
-		name.add(new Nurse("Nurse. Cath Madden", "ER Nurse"));
-		name.add(new Nurse("Nurse. Mavis Elrod", "ER Nurse"));
-		name.add(new Nurse("Nurse. Fiora Hansley", "ER Nurse"));
-		name.add(new Nurse("Nurse. May Monroe ", "Nurse Manager"));
-
-		// Maintenance
-		name.add(new Maintenance("Maintenace, Simon Ajello ", "Preventive Maintenance"));
-		name.add(new Maintenance("Maintenace, Benjamin Barone ", "Corrective maintenance"));
-		name.add(new Maintenance("Maintenace, Lucas Bernard", "Preventive Maintenance"));
-		name.add(new Maintenance("Maintenace, Noah Santos", "Corrective maintenance"));
-		name.add(new Maintenance("Maintenace, Liam De Leon ", "Preventive Maintenance"));
-		name.add(new Maintenance("Maintenace, Oliver Cruz ", "Corrective maintenance"));
-		name.add(new Maintenance("Maintenace, William Aquino", "Predictive Maintenance"));
-		name.add(new Maintenance("Maintenace, Henry Chavez", "Predictive Maintenance"));
-		name.add(new Maintenance("Maintenace, Mateo Abalos", "Hard Floor Maintenance"));
-		name.add(new Maintenance("Maintenace, Ethan Gonzales", "Hard Floor Maintenance"));
-
-		System.out.println("---------------Staffs And Maintinance---------------");
-
-		System.out.println("Please Choose a Category: ");
-		System.out.println("[1] Doctor");
-		System.out.println("[2] Nurse");
-		System.out.println("[3] Maintenanace");
-
-		Set<Integer> chosenCategories = new HashSet<>();
-
-		while (chosenCategories.size() < 3) {
-			int choice;
-			while (true) {
-				choice = input.nextInt();
-
-				switch (choice) {
-				case 1:
-					System.out.println("\r\n"
-							+ "\t \t \t \t \t \t \t \t \t \t ██████╗  ██████╗  ██████╗████████╗ ██████╗ ██████╗ ███████╗\r\n"
-							+ "\t \t \t \t \t \t \t \t \t \t ██╔══██╗██╔═══██╗██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██╔════╝\r\n"
-							+ "\t \t \t \t \t \t \t \t \t \t ██║  ██║██║   ██║██║        ██║   ██║   ██║██████╔╝███████╗\r\n"
-							+ "\t \t \t \t \t \t \t \t \t \t ██║  ██║██║   ██║██║        ██║   ██║   ██║██╔══██╗╚════██║\r\n"
-							+ "\t \t \t \t \t \t \t \t \t \t ██████╔╝╚██████╔╝╚██████╗   ██║   ╚██████╔╝██║  ██║███████║\r\n"
-							+ "\t \t \t \t \t \t \t \t \t \t ╚═════╝  ╚═════╝  ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝\r\n"
-							+ "");
-					for (Staff staffs : name) {
-						if (staffs instanceof Doctor) {
-							staffs.displayInfo();
-							System.out.println();
-						}
-					}
-
-					break;
-
-				case 2:
-					System.out.println("---------- NURSE ----------");
-					for (Staff staffs : name) {
-						if (staffs instanceof Nurse) {
-							staffs.displayInfo();
-							System.out.println();
-
-						}
-					}
-
-					break;
-
-				case 3:
-					System.out.println("---------- MAINTENANCE ----------");
-					for (Staff staffs : name) {
-						if (staffs instanceof Maintenance) {
-							staffs.displayInfo();
-							System.out.println();
-
-						}
-					}
-
-				default:
-					System.out.println("------------------------------------");
-
-				}
-				if (chosenCategories.add(choice)) {
-					break;
-				} else {
-					System.out.println("Category already chosen. Please choose a different category.");
-
-				}
-
-			}
-		}
-	}
+        while (chosenCategories.size() < 3) {
+            int choice;
+            while (true) {
+       choice = input.nextInt();
+            
+          switch (choice){
+        case 1:
+            System.out.println("---------- DOCTORS ----------");
+          for (Staffs staffs : name ) {
+            if (staffs instanceof Doctor) {
+                staffs.displayInfo();
+                System.out.println();
+            }
+        }
+          for (Staffs staffs : name ) {
+            if (staffs instanceof Doctor1) {
+                staffs.displayInfo();
+                System.out.println();
+                
+            }
+        }
+          for (Staffs staffs : name ) {
+            if (staffs instanceof Doctor2) {
+                staffs.displayInfo();
+                System.out.println();
+           }
+      }
+          break;
+          
+        case 2:
+                   System.out.println("---------- NURSE ----------");
+            for (Staffs staffs : name ) {
+            if (staffs instanceof Nurse) {
+                staffs.displayInfo();
+                System.out.println();
+          
+            }
+       }
+            for (Staffs staffs : name ) {
+            if (staffs instanceof Nurse1) {
+                staffs.displayInfo();
+                System.out.println();
+          
+            }
+       }
+            for (Staffs staffs : name ) {
+            if (staffs instanceof Nurse2) {
+                staffs.displayInfo();
+                System.out.println();
+          
+            }
+        }
+          break;
+          
+        case 3:
+                   System.out.println("---------- MAINTENANCE ----------");
+            for (Staffs staffs : name ) {
+            if (staffs instanceof Maintenance) {
+                staffs.displayInfo();
+                System.out.println();
+          
+            }
+        }
+            for (Staffs staffs : name ) {
+            if (staffs instanceof Maintenance1) {
+                staffs.displayInfo();
+                System.out.println();
+            break;
+            }
+        }      
+            
+            for (Staffs staffs : name ) {
+            if (staffs instanceof Maintenance2) {
+                staffs.displayInfo();
+                System.out.println();
+            
+            }
+        }     
+        default:
+            System.out.println("------------------------------------");
+            
+        }
+            if (chosenCategories.add(choice)) {
+                    break;  
+                } else {
+                    System.out.println("Category already chosen. Please choose a different category.");
+            
+                }
+      input.close();
+          
+           }
+       }
+    }
 }
+      
+     
+    
+        
+    
+
+   
+    
+    
