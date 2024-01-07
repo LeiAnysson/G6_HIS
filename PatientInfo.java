7845b4ceacef4b744e897aa4015371ad0f6bf74b
cedcedie <99469318+cedcedie@users.noreply.github.com>
2024-01-07 15:14:26 +0800

    PatientInfo.java

--- PatientInfo.java
+++ PatientInfo.java
@@ -1,5 +1,25 @@
 package oopTp;
+class PatientInfo {
+    private static int patientIDs = 1;
+    private String id;
+    private String name;
+    private int age;
+    private String address;
+    private String contactNumber;
 
-public class PatientInfo {
+    public PatientInfo(String name, int age, String address, String contactNumber) {
+        this.id = generatePatientID();
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.contactNumber = contactNumber;
+    }
 
+    private String generatePatientID() {
+        return "P" + patientIDs++;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
