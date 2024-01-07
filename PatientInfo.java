86d8ae03bb1180d37e6b4c536186fb8c8315116c
cedcedie <99469318+cedcedie@users.noreply.github.com>
2024-01-07 23:27:22 +0800

    Update PatientInfo.java

--- PatientInfo.java
+++ PatientInfo.java
@@ -1,25 +1,62 @@
 package oopTp;
-class PatientInfo {
-    private static int patientIDs = 1;
+ private static int patientIDs = 1;
+
     private String id;
     private String name;
     private int age;
     private String address;
     private String contactNumber;
+    private boolean admitted;
+    private boolean discharged;
 
     public PatientInfo(String name, int age, String address, String contactNumber) {
-        this.id = generatePatientID();
+        this.id = patientID();
         this.name = name;
         this.age = age;
         this.address = address;
         this.contactNumber = contactNumber;
+        this.admitted = false;
+        this.discharged = false;
     }
 
-    private String generatePatientID() {
+    private String patientID() {
         return "P" + patientIDs++;
     }
 
     public String getId() {
         return id;
+    }
+
+    public boolean isAdmitted() {
+        return admitted;
+    }
+
+    public boolean isDischarged() {
+        return discharged;
+    }
+
+    public void admitPatient() {
+        admitted = true;
+        System.out.println("Patient Was Admitted Successfully!");
+    }
+
+    public void dischargePatient() {
+        discharged = true;
+        System.out.println("Patient Was Discharged Successfully!");
+    }
+
+    public String getAdmissionStatus() {
+        return admitted ? "Admitted" : "Not Admitted";
+    }
+
+    public String getDischargeStatus() {
+        return discharged ? "Discharged" : "Not Discharged";
+    }
+
+    public void displayPatientInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+        System.out.println("Contact Number: " + contactNumber);
     }
 }
