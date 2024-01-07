1011f7827aafd7a60f7a5aeafd51bb404080292f
cedcedie <99469318+cedcedie@users.noreply.github.com>
2024-01-07 15:12:45 +0800

    Billing.java

--- Billing.java
+++ Billing.java
@@ -1,5 +1,15 @@
 package oopTp;
+class Billing {
+    private PatientInfo patientInfo;
 
-public class Billing {
+    public Billing(PatientInfo patientInfo) {
+        this.patientInfo = patientInfo;
+    }
 
+    public void displayBillingInfo() {
+        System.out.println("Billing Information for Patient ID: " + patientInfo.getId());
+
+      // kung paano lalabas billing 
+       
+    }
 }
