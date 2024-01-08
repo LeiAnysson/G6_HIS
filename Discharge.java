b5e9b9f8bc8c7f84932c27ee8b1cbd997d40fe7e
patotiee <155729371+aliesterkyutie@users.noreply.github.com>
2024-01-08 17:41:57 +0800

    Update Discharge.java

--- Discharge.java
+++ Discharge.java
@@ -31,19 +31,16 @@ public class Discharge {
 		System.out.println("");
 
 		System.out.println("----------Your Discharge Info----------");
-		System.out.println("Name: " + fname + "" + lname);
-		System.out.println("Phone Number: " + "" + pn );
-		System.out.println("Facility Name: " + fcn);
-		System.out.println("");
-		System.out.println("----------Attending Physician----------");
-		System.out.println("Name: " + fnm + "" + lnm);
-		System.out.println("");
-		System.out.println("At admission you presented with the following symptomps: " );
-		System.out.println("- Fever");
-		System.out.println("- Headache");
-		System.out.println("- Difficulty of breathing");
-		System.out.println("You where diagnosed with : " + dn);
-		System.out.println("You where treated with:  " + td);
-		System.out.println("Your test were (include result): " + ts);
+                System.out.println("Name: " + fname + "" + lname);
+                System.out.println("Phone Number: " + "" + pn );
+                System.out.println("Facility Name: " + fcn);
+                System.out.println("");
+                System.out.println("----------Attending Physician----------");
+                System.out.println("Name: " + fnm + "" + lnm);
+                System.out.println("");
+                System.out.println("At admission you presented with the following symptomps: " + st );
+                System.out.println("You where diagnosed with : " + dn);
+                System.out.println("You where treated with:  " + td);
+                System.out.println("Your test were (include result): " + ts);
 	}       
 }
