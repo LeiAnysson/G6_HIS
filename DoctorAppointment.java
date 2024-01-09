83db4b8f37e79c5f60e99b65650b7a6eedd20e6b
Adrianne Lang <137845861+AdrianneVilla@users.noreply.github.com>
2024-01-10 01:18:15 +0800

    Add files via upload

--- DoctorAppointment.java
+++ DoctorAppointment.java
@@ -4,20 +4,18 @@ import java.util.*;
 public class DoctorAppointment extends PatientAppointment {
 	static TreeMap<String, String> list = new TreeMap<>(appointmentList);
 	static TreeMap<String, String> cList = new TreeMap<>(AppointmentList);
+
 	public static void doctorAppointment() {
 		System.out.println("LIST OF APPOINTMENTS \n");
-	
 
 		for (Map.Entry<String, String> entry : appointmentList.entrySet()) {
 			String key = entry.getKey().toString();
-			System.out.println("Appointment Number: "+entry.getKey());
+			System.out.println("Appointment Number: " + entry.getKey());
 			System.out.println("Patient Name: " + list.get(key));
+			System.out.println("Doctor: " + getChosen());
 
 		}
-		for (Map.Entry<String, String> entry : staffName.entrySet()) {
-			String key = entry.getKey().toString();
-			System.out.println("Doctor : Dr. " + entry.getValue());
-		}
+
 	}
 
 }
