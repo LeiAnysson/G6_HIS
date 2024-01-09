478c16850ee97c4c211d5347a5ba4057e95d29cf
cedcedie <99469318+cedcedie@users.noreply.github.com>
2024-01-10 05:23:56 +0800

    Update DoctorAppointment.java

--- DoctorAppointment.java
+++ DoctorAppointment.java
@@ -6,13 +6,13 @@ public class DoctorAppointment extends PatientAppointment {
 	static TreeMap<String, String> cList = new TreeMap<>(AppointmentList);
 	
 	public static void doctorAppointment() {
-		System.out.println("LIST OF APPOINTMENTS");
+		System.out.println("\t \t \t \t \t \t \t \t \t \t \t \t  LIST OF APPOINTMENTS");
 
 		for (Map.Entry<String, String> entry : appointmentList.entrySet()) {
 			String key = entry.getKey().toString();
-			System.out.println("\nAppointment Number: "+ entry.getKey());
-			System.out.println("Patient Name: " + appointmentList.get(key));
-			System.out.println("Doctor : Dr. " + getChosen());
+			System.out.println("\n\t \t \t \t \t \t \t \t \t \t \t \t  Appointment Number: "+ entry.getKey());
+			System.out.println("\t \t \t \t \t \t \t \t \t \t \t \t  Patient Name: " + appointmentList.get(key));
+			System.out.println("\t \t \t \t \t \t \t \t \t \t \t \t  Doctor : Dr. " + getChosen());
 		}		
 	}
 }
